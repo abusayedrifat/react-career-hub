0099e6e012b34743c978999619e7326cd4aa7d27
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2024-05-02 20:35:08 +0600

    update

---
+++ src/components/AppliedJob/AppliedJob.jsx
@@ -0,0 +1,11 @@
+
+
+const AppliedJob = () => {
+    return (
+        <div>
+            <h1>This is applied job page</h1>
+        </div>
+    );
+};
+
+export default AppliedJob;
---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div className="text-center font-extrabold left-[50%] top-[50%] translate-x-[50%] translate-y-[50%] text-4xl">
+            <h1>Oops! This site can't be found</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
