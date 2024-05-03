646919e974bd17de221a9fe5b9fad1c88012d376
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2024-05-03 08:28:19 +0600

    update

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,11 @@
+
+
+const CategoryList = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-bold text-center">Job Category List</h1>
+        </div>
+    );
+};
+
+export default CategoryList;
