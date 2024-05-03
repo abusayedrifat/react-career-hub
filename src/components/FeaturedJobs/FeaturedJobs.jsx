3b835d8b8d01ca5f622da89c00c65a0817d4baa8
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2024-05-03 08:33:02 +0600

    update

---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FeaturedJobs = () => {
+    return (
+        <div>
+            <h1 className='text-5xl font-bold text-center'>Featured Jobs</h1>
+        </div>
+    );
+};
+
+export default FeaturedJobs;
